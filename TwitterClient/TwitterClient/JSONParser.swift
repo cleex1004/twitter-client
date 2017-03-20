0b58ae6f67e682a97ec2ec6385d1fa7b7b070046
cleex1004 <cleex1004@gmail.com>
2017-03-20 13:11:18 -0700

    completed in class

---
+++ TwitterClient/TwitterClient/JSONParser.swift
@@ -0,0 +1,18 @@
+//
+//  JSONParser.swift
+//  TwitterClient
+//
+//  Created by Christina Lee on 3/20/17.
+//  Copyright © 2017 Christina Lee. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONParserCallback = (Bool, [Tweet]?) -> ()
+
+class JSONParser{
+    
+    class func tweetsFrom(data: Data, callback: JSONParserCallback){
+        
+    }
+}
